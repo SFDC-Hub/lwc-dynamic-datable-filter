9bf68b56bec154ddd8bfaeb50fbf45c72d50297a
Abhishek Patil <patilabhishek168@gmail.com>
2024-03-02 21:06:51 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# lwc-dynamic-filter
-Dynamic filter in LWC
+# Dynamic Filter Lightning Web Component
+
+This Lightning Web Component (LWC) provides dynamic filtering functionality for data tables in Salesforce. It generates filters based on a provided JSON configuration and allows users to filter data in the table accordingly.
+
+## Installation
+
+To use this component in your Salesforce org, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Deploy the component to your Salesforce org using Salesforce CLI or Salesforce Extension for Visual Studio Code.
+3. Use the component in your Lightning pages or Lightning communities.
+
+## Usage
+
+To use the Dynamic Filter component in your Salesforce org:
+
+1. Include the component in your Lightning page or Lightning community using the following syntax:
+
+`<c-dynamic-filter filters={filters} tdata={tableData} onfilter={handleFilter}></c-dynamic-filter>`
+
+
+2. Provide the necessary attributes to the component:
+   - `filters`: JSON array containing field attribute configurations for filters.
+   - `tdata`: JSON array containing the original data for the table.
+   - `onfilter`: Event handler for receiving filtered data from the component.
+
+3. Handle the `onfilter` event in your JavaScript controller to receive filtered data from the component.
+
+## Attributes
+
+The Dynamic Filter component accepts the following attributes:
+
+- `filters`: JSON array containing field attribute configurations for filters. Each filter object should include the following attributes:
+  - `api`: API name of the field in Datatable.
+  - `name`: Name of the field.
+  - `type`: Type of the field (text, number, picklist, date, datetime, color).
+  - `label`: Label of the field in the filter.
+- `tdata`: JSON array containing the original data for the table.
+
+## Example
+
+Here's an example of how to use the Dynamic Filter component:
+
+```javascript
+// Define filters configuration
+const filters = [
+    { api: 'name', name: 'name', type: 'text', label: 'Name' },
+    { api: 'status', name: 'status', type: 'picklist', label: 'Status'}
+];
+
+
+// Handle filter event
+handleFilter(event) {
+    //Reassign the Table Data with filtered data:
+    this.tdata = event.detail.data;
+}
+```
+
+# Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+
---
+++ force-app/main/default/lwc/dynamicFilter/dynamicFilter.js
@@ -0,0 +1,91 @@
+import { LightningElement, api } from 'lwc';
+export default class DynamicFilter extends LightningElement {
+    @api filters;
+    @api tdata;
+    originalTdata;
+
+    connectedCallback() {
+        this.originalTdata = JSON.parse(JSON.stringify(this.tdata));
+        this.filters = JSON.parse(JSON.stringify(this.filters));
+        this.filters.forEach(item => {
+            item[item.type] = true;
+            if(item.type == 'text' || item.type == 'number' || item.type == 'tel' || item.type == 'date' || item.type == 'datetime' || item.type == 'color'){
+                item.stdIp = true;   
+            }
+            if(item.type == 'picklist'){
+                item.options = [];   
+            }
+        });
+
+        this.originalTdata.forEach(item => {
+            this.filters.forEach(filter => {
+                if (filter.type === 'picklist') {
+                    const value = item[filter.api];
+                    if (!filter.options.includes(value) && value!=null) {
+                        filter.options.push(value);
+                    }
+                }
+            });
+        });
+
+        this.filters.forEach(filter => {
+            if (filter.type === 'picklist') {
+                filter.options.sort();
+                filter.options = [{label:'-Select-', value:null}].concat(filter.options.map(item => ({label:String(item), value:String(item)})));
+            }
+        });
+    }
+
+    handleChange(event) {
+        this.filters.find(filter => filter.name === event.target.name).value = event.target.value;
+        this.filterData();
+    }
+
+    filterData(){
+        let data = this.originalTdata.filter(item => {
+            return this.filters.every(filter => {
+                if (!filter.value) {
+                    return true;
+                }
+                if(filter.type == 'text' || filter.type == 'picklist' || item.type == 'number' || item.type == 'tel'){
+                    return String(String(item[filter.api])).toLowerCase().includes(String(filter.value).toLowerCase());
+                }else if(item.type == 'date' || item.type == 'datetime' || item.type == 'color'){
+                    return String(item[filter.api]) == (filter.value);
+                }
+            });
+        });
+        this.sendData(data);
+    }
+
+    sendData(data){
+        this.dispatchEvent(new CustomEvent('filter', { detail: { data } }));
+    }
+
+    resetFilters(){
+        // console.log(JSON.stringify(this.filters));
+        this.filters.forEach(filter => {
+            
+            if(filter.type == 'text'){
+                filter.value = '';
+            }
+            if(filter.type == 'picklist'){
+                filter.value = null;
+            }
+        });
+
+        let textInputs = this.template.querySelectorAll('lightning-input');
+        textInputs.forEach(input => {
+            console.log("clearing values");
+            input.value = '';
+        });
+        let picklistInputs = this.template.querySelectorAll('lightning-combobox');
+        picklistInputs.forEach(input => {
+            console.log("clearing values");
+            input.value = null;
+        });
+        
+        // console.log(JSON.stringify(this.filters));
+        this.filters = JSON.parse(JSON.stringify(this.filters));
+        this.sendData(this.originalTdata);
+    }
+}
